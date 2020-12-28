0a374cc258ed748620e3f3055a46962c68d08628
l1ubomyr <lezya5954@gmail.com>
2020-12-28 18:17:21 +0200

    js

---
+++ Ch_Liubomyr1/JS/index.js
@@ -0,0 +1,18 @@
+var years = confirm('ви повнолітній?');
+    console.log(years);  
+if (years) {
+    alert = confirm ("чи вживаєте ви алкоголь");
+    alert = confirm ("курите?");
+    alert = confirm ("займаєтеся спортом?");
+    alert = prompt ("Скільки вам років?");
+}else{
+    var old = prompt ("скільки вам років");
+    console.log(old);
+    if (old < 18){
+       alert ("Де твої Ancestors") 
+}else{ (old < 10) 
+        alert ("PIZDEC")
+    
+}
+}
+
