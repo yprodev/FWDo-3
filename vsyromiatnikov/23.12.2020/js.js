28427161116ecd533392da849050f52fa8e1cb23
вфыывв виталий <sonioni1998@gmail.com>
2020-12-29 21:58:05 +0200

    Add js homework

---
+++ vsyromiatnikov/23.12.2020/js.js
@@ -0,0 +1,54 @@
+
+let old=confirm('Are you adult?');
+old
+if(old){
+let x;
+x=0;
+console.log(x)
+let=alert("Are  you drinking alcohol?");
+x=x+0.1;    
+console.log(x)
+let =alert("Are  you smoking?")
+x=x+0.2;
+console.log(x)
+let =alert("Do yo do Sports?")
+x=x-0.4;
+console.log(x)
+ let old=prompt("How old are you")
+ if(old==60){
+  alert("Sorry but we haven't enough food");
+  let =confirm(x);
+}
+else 
+if(old<=20){
+    alert("Congratulations you are the first student who has money for insurance");
+    let=confirm(x);
+}
+
+else
+if
+(old==30){
+    x=x+0.1;
+    console.log(x);
+    alert("Nice");
+    let=confirm(x);
+}
+
+else    
+if
+(20<30){
+
+    alert("Nothing");
+
+}
+
+}
+else{
+let old=prompt("How old are you")
+if (old<=10)
+ {
+alert("Call your parents");
+} else if (10<18) {
+   alert("Call 073");
+}
+}
